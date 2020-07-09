495a894d45fde5b7a552672b6887c73c6a05c34f
Hamilton Reed <hamiltonjreed@gmail.com>
2020-07-09 14:39:30 -0600

    Stub controllers & add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :home, :path => '/', :only => [:index]
+
+  resources :incident, :only => [:show, :create]
 end
